9f4a666dad9ccd37e63b916de61974516d7a1a09
Yashvardhan Arora <yash22arora@gmail.com>
2022-11-09 01:27:55 +0530

    Add sidenav item, change project logo
    Signed-off-by: Yashvardhan Arora <yash22arora@gmail.com>

--- client/app/components/Sidenav/Sidenav.tsx
+++ client/app/components/Sidenav/Sidenav.tsx
@@ -5,8 +5,11 @@ import {
   BsBookmarkFill,
   BsPerson,
   BsPersonFill,
+  BsBell,
+  BsBellFill,
 } from "react-icons/bs";
 import { SiGoogleads } from "react-icons/si";
+import { HiCode } from "react-icons/hi";
 import SidenavItem from "./SidenavItem";
 import SidenavItemType from "./types";
 
@@ -24,6 +27,12 @@ const sidenavItems: SidenavItemType[] = [
     path: "/explore",
   },
   {
+    name: "Notifications",
+    icon: <BsBell />,
+    selectedIcon: <BsBellFill />,
+    path: "/notifications",
+  },
+  {
     name: "Saved",
     icon: <BsBookmark />,
     selectedIcon: <BsBookmarkFill />,
@@ -39,10 +48,11 @@ const sidenavItems: SidenavItemType[] = [
 
 const Sidenav: React.FC = () => {
   return (
-    <div className="flex flex-col w-1/4 pl-24 pr-8 border-r border-gray-600 py-8 items-start justify-start h-full">
-      <div className="p-4 pt-0 flex flex-row mb-3">
-        <SiGoogleads className="text-3xl -rotate-90 mr-2" />
-        <SiGoogleads className="text-3xl rotate-90" />
+    <div className="flex flex-col pl-24 pr-16 border-r-[0.5px] border-gray-600 py-8 items-start justify-start h-full">
+      <div className="p-4 pt-0 flex flex-row mb-2">
+        <HiCode size={50} />
+        {/* <SiGoogleads className="text-3xl -rotate-90 mr-2" />
+        <SiGoogleads className="text-3xl rotate-90" /> */}
       </div>
       {sidenavItems.map((item, index) => (
         <SidenavItem item={item} key={index} />
--- client/app/components/Sidenav/SidenavItem.tsx
+++ client/app/components/Sidenav/SidenavItem.tsx
@@ -8,7 +8,7 @@ const SidenavItem: React.FC<SidenavItemProps> = ({ item }) => {
   return (
     <div className="flex flex-row items-center justify-center p-4 rounded-full hover:bg-slate-400 hover:bg-opacity-20 text-xl my-1 cursor-pointer">
       <div className="mr-6 text-2xl">{item.icon}</div>
-      <div className="tracking-wide">{item.name}</div>
+      <div className="tracking-normal">{item.name}</div>
     </div>
   );
 };
