0d192f785d623ab8bd46857aee65b56c1fdcbf7e
Yashvardhan Arora <yash22arora@gmail.com>
2022-11-09 22:51:14 +0530

    Create common components directory, initiate Button component
    Signed-off-by: Yashvardhan Arora <yash22arora@gmail.com>

---
+++ client/app/components/common/Button.tsx
@@ -0,0 +1,7 @@
+enum BUTTON_VARIANT {
+  PRIMARY = "primary", // default, filled bg, white text
+  SECONDARY = "secondary", // border, transparent bg, colored text
+  TERTIARY = "tertiary", // no border, transparent bg, colored text, bg on hover
+}
+
+export {};
