10971ae97c015925a5937ff0dc88135dfcd59e96
darshan <darshpat01@gmail.com>
2023-01-21 18:29:35 +0530

    added roboto font

--- client/app/components/tags/Tags.tsx
+++ client/app/components/tags/Tags.tsx
@@ -1,7 +1,7 @@
 function Tags(props: any) {
   const { children } = props;
   return (
-    <div className="rounded-[25px] text-white p-2 bg-[#E8DEF8] p-3 m-2 h-min w-min  text-black font-[500] tracking-[0.1px] hover:bg-[#f6def8]">
+    <div className="rounded-[25px] bg-[#E8DEF8] p-3 m-2 h-min w-min  text-black font-[500] tracking-[0.1px] hover:bg-[#f6def8]">
       {children}
     </div>
   );
